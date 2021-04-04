d8dcf7613d588dfbdb89b99851d6b29dd157f26d
Duyen Tran <hongduyen.16697@gmail.com>
2021-04-04 01:26:52 -0700

    drive3

--- HW6/option3/driver3.h
+++ HW6/option3/driver3.h
@@ -2,8 +2,64 @@
 
 #include <string>
 #include "Course.h"
+#include"input.h"
+
+using namespace std;
 
 void driver3() 
 {
 	MyBag<Course> courses;
+
+	int numberOfCourses;
+
+	int menuOption;
+	
+	do
+	{
+			system("cls");
+			cout << "\t\t  3) Courses using MyBags of integers, strings, doubles, and chars\n";
+			cout << '\t' << string(40, char(196)) << '\n';
+			cout << "\t\t 1> Specify the number of courses\n";
+			cout << "\t\t 2> Read in data file and insert into courses\n";
+			cout << "\t\t 3> Search for a student record from a course\n";
+			cout << "\t\t 4> Remove a student record from a course\n";
+			cout << "\t\t 5> Display course(s)\n";
+			cout << "\t\t 0> return\n";
+			cout << '\t' << string(40, char(196)) << '\n';
+
+			menuOption = inputInteger("Option: ", 0, 5);
+
+			switch (menuOption)
+			{
+			case 0: 
+				break;
+			case 1: 
+			{
+				cout << " Enter the number of courses: ";
+				cin >> numberOfCourses;
+				cout << '\n';
+
+				cout << numberOfCourses << " Course(s) has been created. \n";
+				break;
+			}
+
+			case 2: 
+			{
+
+			}
+
+			case 3: 
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default: cout << "\t\t    ERROR-Invalid Option. Please re-enter."; break;
+			}
+
+		cout << '\n';
+		system("pause");
+
+	} while (true);
+
 }
